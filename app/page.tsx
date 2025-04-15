e9718271eeaf68b3dfd93a9af92d6831b48231c6
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-04-15 16:17:22 +0300

    Add login page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import { Button } from '@/app/components/ui/button';
+import { LoginForm } from '@/app/components/login-form';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="flex flex-col min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-b from-black via-gray-900 to-black bg-no-repeat bg-center bg-cover">
+      <h1 className="font-[Impact] font-bold text-pink-500 text-[8rem] tracking-wide">
+        FIGHT CLUB
+      </h1>
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </main>
   );
 }
